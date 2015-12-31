6fb2874bda9b59067678d92d22dde639cc23b250
Casey <dpoyesac@gmail.com>
2015-12-30 17:18:38 -0700

    added basic formatting & exception handling

--- fandango_soup.py
+++ fandango_soup.py
@@ -10,17 +10,22 @@
 # -----------------------------------------------------------------------------
 
 import requests
+import datetime
 from bs4 import BeautifulSoup as bs
 
-targs = ["http://www.fandango.com/80521_movietimes",
-         "http://www.fandango.com/80521_movietimes?pn=2"
-         ]
+
+def parse_weird_fandango_date(dt):
+    frmt = "%Y-%m-%dT%H:%M:%S-07:00"
+    return datetime.datetime.strptime(dt, frmt)
 
 
-def main(targets=targs):
+def do_the_work(targets):
     res = {}
     for t in targets:
-        r = requests.get(t)
+        try:
+            r = requests.get(t)
+        except requests.exceptions.RequestException as e:
+            raise e
         soup = bs(r.text, "lxml")
         theaters = soup.find_all("div",
                                  itemtype="http://schema.org/MovieTheater")
@@ -35,14 +40,33 @@ def main(targets=targs):
                 temp = m.find_all("meta", itemprop="startDate")
                 dic[nam] = []
                 for t in temp:
-                    dic[nam].append(t["content"])
+                    dic[nam].append(parse_weird_fandango_date(t["content"]))
             res[name] = dic
+    return res
+
+
+def printer(results):
+    for th in results.keys():
+        print('-' * 70)
+        print(th)
+        for m in results[th].keys():
+            times = map(lambda x: x.strftime("%I:%M %p"), results[th][m])
+            res = "\t{}:".format(m)
+            for t in times:
+                res = "{} {}".format(res, t)
+            print(res)
+            #  print('\t', m, times)
+        print()
+
+
+def main():
+    targs = ["http://www.fandango.com/80521_movietimes",
+             "http://www.fandango.com/80521_movietimes?pn=2"
+             ]
+    res = do_the_work(targs)
     return res
 
 
 if __name__ == '__main__':
     thunder = main()
-    for th in thunder.keys():
-        print(th)
-        for m in thunder[th].keys():
-            print('\t', m, thunder[th][m])
+    printer(thunder)
