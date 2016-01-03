4856ef3d5fa274f1283fd268cc411b687fa9a84a
Casey <dpoyesac@gmail.com>
2016-01-03 16:34:49 -0700

    added basic time parsing and color coding

--- fandango_soup.py
+++ fandango_soup.py
@@ -16,6 +16,55 @@ import shelve
 from bs4 import BeautifulSoup as bs
 
 
+def get_colors(color_flag=True):
+    """ Return object with color information.
+    """
+    class Colors:
+        pass
+    colors = Colors()
+    # # else:
+    #     # https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+    #     # \033[_;__;__m     --> first:  character effect
+    #     #                       second: foreground color
+    #     #                       third:  background color
+    #     # \033[38;5;___m    --> extended foreground color (0...255)
+    #     # \033[48;5;___m    --> extended background color (0...255)
+    color_info = {'bold':   "\033[1m",
+                  'italic': "\033[3m",
+                  #  'temp':   "\033[1;34;47m",
+                  #  'wind':   "\033[38;5;199m\033[48;5;157m",
+                  #  'high':   "\033[1;34;47m",
+                  'early':   "\033[1;34;47m",
+                  #  'low':    "\033[1;34;47m",
+                  #  'cond':   "\033[3;36;47m",
+                  'clear':  "\033[0m",
+                  #  'hot':    "\033[38;5;160m\033[48;5;007m",
+                  #  'cool':   "\033[38;5;020m\033[48;5;155m",
+                  #  'night':  "\033[38;5;015m\033[48;5;017m",
+                  #  'dusk':   "\033[38;5;015m\033[48;5;020m",
+                  #  'dawn':   "\033[38;5;000m\033[48;5;172m",
+                  #  'day':    "\033[38;5;000m\033[48;5;226m",
+                  #  'cloud25':    "\033[38;5;015m\033[48;5;012m",
+                  'hour':    "\033[38;5;015m\033[48;5;012m",
+                  #  'cloud50':    "\033[38;5;015m\033[48;5;067m",
+                  #  'cloud75':    "\033[38;5;015m\033[48;5;246m",
+                  #  'cloud100':   "\033[38;5;018m\033[48;5;255m",
+                  #  'precip25':   "\033[38;5;232m\033[48;5;255m",
+                  #  'precip50':   "\033[38;5;238m\033[48;5;255m",
+                  #  'precip75':   "\033[38;5;250m\033[48;5;232m",
+                  #  'precip100':  "\033[38;5;255m\033[48;5;232m",
+                  'grey_background': "\033[38;5;233m\033[48;5;251m"
+                  }
+    for key in color_info.keys():
+        if color_flag is True:
+            setattr(colors, key, color_info[key])
+        elif color_flag is False:
+            setattr(colors, key, '')
+        else:
+            raise KeyError("color_flag is unset")
+    return colors
+
+
 def today_formatted():
     today = datetime.date.today()
     return today.strftime("%b_%d_%y")
@@ -23,8 +72,12 @@ def today_formatted():
 
 def parse_weird_fandango_date(dt):
     # dammit fandango, whats with the colon!
-    dt_n = dt[:-3] + dt[-2:]
-    frmt = "%Y-%m-%dT%H:%M:%S%z"
+    # here we have the format string WITH timezone
+    #  dt_n = dt[:-3] + dt[-2:]
+    #  frmt = "%Y-%m-%dT%H:%M:%S%z"
+    # here we have the format string WITHOUT timezone
+    dt_n = dt.rsplit('-', 1)[0]
+    frmt = "%Y-%m-%dT%H:%M:%S"
     return datetime.datetime.strptime(dt_n, frmt)
 
 
@@ -63,11 +116,28 @@ def do_the_work(soup):
 
 
 def printer(results):
+    COLOR = get_colors()
+    now = datetime.datetime.today()
+    soon = now + datetime.timedelta(hours=1)
     for th in results.keys():
         print('-' * 70)
         print(th)
         for m in results[th].keys():
-            times = map(lambda x: x.strftime("%I:%M %p"), results[th][m])
+            #  times = map(lambda x: x.strftime("%I:%M %p"), results[th][m])
+
+            times_raw = results[th][m]
+            times = []
+            for tm in times_raw:
+                t = tm.replace(tzinfo=None)
+                if t < now:
+                    c = COLOR.early + COLOR.italic
+                elif t < soon:
+                    c = COLOR.hour
+                else:
+                    c = COLOR.clear
+                times.append("{}{}{}".format(c,
+                                             t.strftime("%I:%M %p"),
+                                             COLOR.clear))
             res = "\t{}:".format(m)
             for t in times:
                 res = "{} {}".format(res, t)
